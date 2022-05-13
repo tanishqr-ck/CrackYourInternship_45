52e082d29a4b6f5a377e22ef23a063344c71c4da
Tanishq Rajput <ertanishqrajput@gmail.com>
2022-05-13 18:56:20 +0530

    Arrays

---
+++ Day 1/removeduplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0,j=1;
+        while(i<nums.size()&&j<nums.size())
+        {
+            if(nums[j]!=nums[i])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+            j++;
+        }
+        return i+1;
+        
+    }
+};
