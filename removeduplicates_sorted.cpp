c3c4b2b1b166a55943296209fd2b76859b4d2fc7
Tanishq Rajput <ertanishqrajput@gmail.com>
2022-05-25 11:50:33 +0530

    Solution for sorted array

---
+++ removeduplicates_sorted.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    int k=0;
+        for(int i=1;i<nums.size();i++)
+    {
+       if(nums[i]!=nums[i-1])
+       {
+         nums[++k]=nums[i];  
+       }
+        
+    }
+        return k+1;}
+};
